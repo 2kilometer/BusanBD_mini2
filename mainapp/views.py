37f9c68118c9343cc14f75656e4d4294776a41c1
jeonsungsam <sungsam9709@gmail.com>
2023-07-23 22:28:16 +0900

    일단 저장

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .DB_Sql import disease
 
 # Create your views here.
 
@@ -12,10 +13,25 @@ def index(request):
 
 
 def Register(request):
+    age = request.POST.get("age", "")
+    dis_list = disease.dis_list()
+    dis_middle = disease.dis_middle()
+    if age != "":
+        dis_age = disease.dis_age(age)
+    else:
+        dis_age = disease.dis_age('10대')
+    return render(request,
+                  "mainapp/register.html",
+                  {"dis_list" : dis_list,
+                   "dis_middle" : dis_middle,
+                   "dis_age" : dis_age,})
+
+def Register1(request) :    
     return render(request,
                   "mainapp/register.html",
                   {})
-    
+
+
 def Data_info(request):
     return render(request,
                   "mainapp/data_info.html",
