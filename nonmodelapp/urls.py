ba8d9dadeeadb69102be5d08ba432a44e25e8a89
jeonsungsam <sungsam9709@gmail.com>
2023-07-25 11:49:36 +0900

    ㄷㄷㄷ

--- nonmodelapp/urls.py
+++ nonmodelapp/urls.py
@@ -4,6 +4,17 @@ from django.urls import path
 # - 라이브러리 읽어들이는 것과 동일
 from . import views
 urlpatterns = [
-    
-    
+     # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('index/', views.index),    
+    # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('about/', views.about),    
+    # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('contact/', views.contact),    
+    # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('doctor/', views.doctor),    
+    # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('testimonial/', views.testimonial),    
+    # - http://127.0.0.1:8000/nonmodelapp/index/
+    path('treatment/', views.treatment),    
+
 ]
