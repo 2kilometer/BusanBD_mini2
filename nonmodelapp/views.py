5ac18d759eb564c06b9eb1af17ffb9e620fafebe
jeonsungsam <sungsam9709@gmail.com>
2023-07-25 13:50:41 +0900

    커밋커밋

--- nonmodelapp/views.py
+++ nonmodelapp/views.py
@@ -41,3 +41,62 @@ def treatment(request):
     return render(request,
                   "nonmodelapp/treatment.html",
                   {})
+    
+def Register1(request):
+    # POST 데이터로부터 나이(age) 가져오기
+    age = request.GET.get("age", "")
+    gender = request.GET.get("gender", "")
+
+    if age != "" and gender != "":
+        dis_age = disease.dis_age(age, gender)
+    
+    else :
+        dis_age = age
+    
+
+    return render(request, "nonmodelapp/register1.html", {"dis_age": dis_age})
+
+def sign(request):
+    try :
+        email   = request.POST.get("email","")
+        password   = request.POST.get("password","")
+        age = request.POST.get("age","")
+        gender   = request.POST.get("gender","")
+        stress   = request.POST.get("stress","")
+        dis_id   = request.POST.getlist("dis_id","")
+        dis_id2   = request.POST.getlist("dis_id2","")
+        dis_middle   = request.POST.getlist("dis_middle","")
+        
+        if email != "":
+            sign_in.sign_up(email, password, age, gender, stress)
+            
+            total_dis_id = dis_id + dis_id2
+            
+            sum_dis = list(set(total_dis_id))
+            
+            dis_middle = list(set(dis_middle))
+            
+            for i in range(len(sum_dis)):
+                sign_in.ud_dis(email ,sum_dis[i])
+            
+            for d in range(len(dis_middle)):
+                sign_in.middle(email ,dis_middle[d])
+        
+        
+    except :        
+        ### 오류처리
+        msg = """
+            <script type='text/javascript'>
+                alert('오류발생{}{}');
+            </script>
+        """
+        return HttpResponse(msg)
+    
+    ### 정상처리
+    msg = """
+        <script type='text/javascript'>
+            alert('정상적으로 입력되었습니다!!');
+            history.go(-1);
+        </script>
+    """   
+    return HttpResponse(msg)
